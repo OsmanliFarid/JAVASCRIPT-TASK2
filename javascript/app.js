1eb68525094c1ae39d4bc7324f0dad32a37f68fa
OsmanliFarid <osmanliferid686@gmail.com>
2025-02-21 06:25:13 +0400

    new js and html change

---
+++ javascript/app.js
@@ -0,0 +1,20 @@
+let hefte = +prompt("heftenin necenci gunudu?")
+let gun = ""
+if(hefte === 1) {
+    gun = "Bazar ertesi"
+}else if(hefte === 2) {
+    gun = "Cersenbe axsami"
+}else if(hefte === 3) {
+    gun = "Cersenbe"
+}else if(hefte === 4) {
+    gun = "Cume axsami"
+}else if(hefte === 5) {
+    gun = "Cume"
+}else if(hefte === 6) {
+    gun = "senbe"
+}else if(hefte === 7) {
+    gun = "bazar"
+}else(
+    console.log("bele bir gun yoxdu")
+)
+console.log(gun);
