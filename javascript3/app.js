08a7c2f0ed61e4821175395e2fc20f9368b8d96b
OsmanliFarid <osmanliferid686@gmail.com>
2025-02-21 06:55:50 +0400

    task3 end

---
+++ javascript3/app.js
@@ -0,0 +1,13 @@
+const name = prompt("adin nedir")
+const gender = prompt("cinsiyetin nedir (k/q)")
+
+if (gender === "k" || gender === "K"){
+    console.log(`Salam Cenab ${name}`);
+    
+}else if(gender === "q" || gender === "Q"){
+    console.log(`Salam xanim ${name}`);
+    
+}else{
+    console.log("lufen doru secin");
+    
+}
